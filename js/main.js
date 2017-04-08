a5db80512304929523b1e0fb41098549a1396c64
James Hines <jameshines10@gmail.com>
2017-04-08 14:37:02 -0400

    Subscribe the click counter view component to increment count events

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ let decrementBtn = new DecrementButton(container, eventStore);
 
 container = document.getElementById('counter-view');
 let counterView = new CounterView(container, eventStore);
+counterView.subscribe('sync', 'component.increment.count');
 
 incrementBtn.render();
 decrementBtn.render();
